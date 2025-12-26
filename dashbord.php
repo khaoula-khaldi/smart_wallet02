62fb5b5d6bc2dc6c1d43d0c45be68c39b7a1fd9b
khaoula khaldi <khaoula2417@gmail.com>
2025-12-26 21:54:32 +0100

    ajouter page dashbord

---
+++ dashbord.php
@@ -0,0 +1,154 @@
+<?php
+    require_once 'classUsers.php';
+    if(!isset($_SESSION['user_id'])){
+    header('Location: login.php');
+    exit;
+}
+$user_id=$_SESSION['user_id'];
+
+$afficahgeExObj=new expenses ($pdo);
+$rowEx=$afficahgeExObj->affichageEx($user_id);
+
+$afficahgeExObj=new incomes ($pdo);
+$rowIn=$afficahgeExObj->affichageIn($user_id);
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <script src="https://cdn.tailwindcss.com"></script>
+    <title>dashbord</title>
+</head>
+<body class=" flex flex-col gap-5 font-sans text-gray-800 bg-purple-100">
+
+
+          <nav class="w-full shadow-md p-4 mb-6 bg-purple-200">
+            <h1 class="text-center text-2xl font-bold text-gray-800">Gestion Financière</h1>
+            <p>Bonjour <?php echo $_SESSION['user_nom'];?> </p>
+         </nav>
+        <div class="w-[100%] bg-green-100 border border-gray-200 rounded-xl p-4 shadow-sm flex flex-row justify-between ">
+          <h2 class="text-lg font-bold text-gray-700 ">Menu</h2>
+          <ul class=" flex flex-row  gap-10">
+            <li class="hover:bg-pink-100 transition p-2 rounded cursor-pointer"><a href="dashbord.php">Tableau de bord</a></li>
+            <li class="hover:bg-pink-100 transition p-2 rounded cursor-pointer"><a href="categories.php">Ajouter categories</a></li>
+            <li class="hover:bg-pink-100 transition p-2 rounded cursor-pointer"><a href="incomes.php">Revenu</a></li>
+            <li class="hover:bg-pink-100 transition p-2 rounded cursor-pointer"><a href="expenses.php">Dépenses</a></li>
+            <li class="hover:bg-gray-100 p-2"><a href="deconnexion.php">Déconnexion</a></li>
+            <li class="relative">
+          </ul>
+        </div>
+
+        <main class="flex-1 space-y-8">
+        <section class="flex gap-4">
+          <div class="flex-1 p-4 bg-blue-400 rounded-xl shadow-sm">
+            <p class="font-semibold text-white">Revenu totales </p>
+            <p class="text-2xl font-bold mt-2 text-white"> <?php  $totalIn=new incomes($pdo);
+                                                                  $montantTotalIn=$totalIn->getTotalIncomes($_SESSION['user_id']);
+                                                                  echo $montantTotalIn ;  ?> DH</p>
+          </div>
+          <div class="flex-1 p-4 bg-green-400 rounded-xl shadow-sm">
+            <p class="font-semibold text-white">Dépenses totales</p>
+            <p class="text-2xl font-bold mt-2 text-white"> <?php  $totalEx=new expenses($pdo);
+                                                                  $montantTotalEx=$totalEx->getTotalExpenses($_SESSION['user_id']);
+                                                                  echo $montantTotalEx ;  ?> DH</p>
+            </div>
+          <div class="flex-1 p-4 bg-purple-400 rounded-xl shadow-sm">
+            <p class="font-semibold text-gray-700">Solde</p>
+            <p class="text-2xl font-bold mt-2 text-gray-700">  <?php $solde = $montantTotalIn - $montantTotalEx;
+                                                                      echo $solde;  ?> DH</p>
+          </div>
+        </section>
+      </main>
+
+          <section class="border border-gray-300 rounded-xl shadow-sm bg-blue-100 p-4">
+          <h2 class="text-xl font-bold text-gray-700 mb-4">revenu</h2>
+          <!-- <select name="type" >
+            <option value="revenu">revenu</option>
+            <option value="despenses">despenses</option>
+          </select> -->
+
+          <table class="w-full border border-gray-300 rounded-lg overflow-hidden">
+            <thead class="bg-gray-100 text-gray-700 border-b border-gray-300">
+              <tr>
+                <th class="p-2 border-r border-gray-300">Montant</th>
+                <th class="p-2 border-r border-gray-300">Description</th>
+                <th class="p-2 border-r border-gray-300">Date</th>
+                <th class="p-2">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+                <?php
+        
+                  foreach($rowIn as $aff){
+                             echo"<tr> <td class=\"p-2 border-r border-gray-300\">{$aff['montantIn']}</td>
+                              <td class=\"p-2 border-r border-gray-300\">{$aff['descriptionIn']}</td>
+                              <td class=\"p-2 border-r border-gray-300\">{$aff['dateIn']}</td>
+                              <td class=\"p-2\"> 
+                              <div class=\" flex flex-row gap-2\">
+                              <form action=\"modifierIn.php\" method=\"POST\">
+                              <input name=\"idIn\" type=\"text\" class=\"hidden\" value=\"{$aff['id']}\"/>
+                              <button type=\"submit\" name=\"modifier_incomes\" class=\"bg-green-500 text-white px-3 py-1 rounded hover:bg-green-600 mr-2\">Modifier</button>
+                              </form>
+
+                              <form action=\"supprime.php\" method=\"POST\">
+                              <input name=\"idIn\" type=\"text\" class=\"hidden\" value=\"{$aff['id']}\"/>
+                              <button name=\"delete_incomes\" class=\"bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600\">Supprimer</button>
+                              </form>
+                              </div>
+                              </td>
+                              </tr>";
+                  }
+
+                ?>              
+            </tbody>
+          </table>
+        </section>
+
+
+                <section class="border border-gray-300 rounded-xl shadow-sm bg-blue-100 p-4">
+          <h2 class="text-xl font-bold text-gray-700 mb-4">Expenses</h2>
+
+          <table class="w-full border border-gray-300 rounded-lg overflow-hidden">
+            <thead class="bg-gray-100 text-gray-700 border-b border-gray-300">
+              <tr>
+                <th class="p-2 border-r border-gray-300">Montant</th>
+                <th class="p-2 border-r border-gray-300">Description</th>
+                <th class="p-2 border-r border-gray-300">Date</th>
+                <th class="p-2">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+                <?php
+        
+                  foreach($rowEx as $aff){
+                             echo"<tr> <td class=\"p-2 border-r border-gray-300\">{$aff['montantEx']}</td>
+                              <td class=\"p-2 border-r border-gray-300\">{$aff['descriptionEx']}</td>
+                              <td class=\"p-2 border-r border-gray-300\">{$aff['dateEx']}</td>
+                              <td class=\"p-2\"> 
+                              <div class=\" flex flex-row gap-2\">
+                              <form action=\"modifierEx.php\" method=\"POST\">
+                              <input name=\"              \" type=\"text\" class=\"hidden\" value=\"{$aff['id']}\"/>
+                              <button type=\"submit\" name=\"modifier_incomes\" class=\"bg-green-500 text-white px-3 py-1 rounded hover:bg-green-600 mr-2\">Modifier</button>
+                              </form>
+
+                              <form action=\"supprime.php\" method=\"POST\"> 
+                              <input name=\"idEx\" type=\"text\" class=\"hidden\" value=\"{$aff['id']}\"/>
+                              <button name=\"delete_expenses\" class=\"bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600\">Supprimer</button>
+                              </form>
+                              </div>
+                              </td>
+                              </tr>";
+                  }
+
+                ?>              
+            </tbody>
+          </table>
+        </section>
+
+       
+
+</body>
+</html>
